3adcd5b4f357427e1ee1216075830058579861e9
unknown <T2387@D113.local>
2026-09-24 13:55:00 +0300

    Небольшой рефакторинг

--- BattleshipLib/visual.h
+++ BattleshipLib/visual.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "game.h"
 
-void output(GameField gamefield) {
+void output(std::vector<std::vector<CellState>> gamefield) {
+	for (const std::vector<std::vector<CellState>> row : gamefield) {
+		for (const std::vector<std::vector<CellState>> row : gamefield)
+	}
 }
